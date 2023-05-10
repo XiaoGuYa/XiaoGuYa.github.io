bc6533ef3ac1b1cd1b2cf7900d26eac0200b69cf
XiaoGuYa <17870897@qq.com>
2023-05-10 07:47:19 +0000

    Merge branch 'master' of https://github.com/XiaoGuYa/blogs Updated By Github Actions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,67 @@
-!(() => {
-  codeBlockCopy()
-})()
-
-function codeBlockCopy() {
-  const page = document.querySelector('.post')
-  if (page) {
-    page.addEventListener('click', (event) => {
-      const target = event.target
-      if (!target.classList.contains('copy-code')) return
-      const table = target.parentElement.nextElementSibling
-      if (table.tagName === 'TABLE') {
-        const code = table.querySelector('.code')
-        const selection = window.getSelection()
-        selection.selectAllChildren(code)
-        navigator.clipboard ? navigator.clipboard.writeText(selection.toString()) : document.execCommand('copy')
-        selection.removeAllRanges()
-        target.innerText = '已复制'
-        setTimeout(function () {
-          target.innerText = '复制'
-        }, 2000)
-      }
-    })
-  }
-}
+console.log("\n%c Pigeon By 山卜方 %c https://novcu.com ","color:#fff;background:#000;padding:5px 0","color:#fff;background:#666;padding:5px 0");
 //返回顶部
 $("#top_to").hide();$(window).scroll(function(){if($(this).scrollTop()>200){$("#top_to").fadeIn(100)}else{$("#top_to").fadeOut(200)}});$("#top_to").click(function(){$("body,html").animate({scrollTop:0},400);return false});
+
+
+function codeBlockCopy() {
+  const e = document.querySelector(".page");
+  e && e.addEventListener("click", (e=>{
+      const t = e.target;
+      if (!t.classList.contains("copy-code"))
+          return;
+      const n = t.parentElement.nextElementSibling;
+      if ("TABLE" === n.tagName) {
+          const e = n.querySelector(".code")
+            , o = window.getSelection();
+          o.selectAllChildren(e),
+          navigator.clipboard ? navigator.clipboard.writeText(o.toString()) : document.execCommand("copy"),
+          o.removeAllRanges(),
+          t.innerText = "COPIED",
+          setTimeout((function() {
+              t.innerText = "COPY"
+          }
+          ), 2e3)
+      }
+  }
+  ))
+};
+function chageTimeFormate() {
+  const timeElements = document.getElementsByTagName("time"), lang = GLOBALCONFIG.lang.time
+  for (var i = 0; i < timeElements.length; i++) {
+      const datetime = timeElements[i].getAttribute("datetime"), timeObj = new Date(datetime), daysDiff = utils.timeDiff(timeObj, new Date())
+      var timeString;
+      if (daysDiff === 0) {
+          timeString = lang.recent;
+      } else if (daysDiff === 1) {
+          timeString = lang.yesterday;
+      } else if (daysDiff === 2) {
+          timeString = lang.berforeyesterday;
+      } else if (daysDiff <= 7) {
+          timeString = daysDiff + lang.daybefore;
+      } else {
+          if (timeObj.getFullYear() !== new Date().getFullYear()) {
+              timeString = timeObj.getFullYear() + "/" + (timeObj.getMonth() + 1) + "/" + timeObj.getDate();
+          } else {
+              timeString = (timeObj.getMonth() + 1) + "/" + timeObj.getDate();
+          }
+      }
+      timeElements[i].textContent = timeString;
+  }
+};
+const utils = {
+    timeDiff: (timeObj, today) => {
+        var timeDiff = today.getTime() - timeObj.getTime();
+        return Math.floor(timeDiff / (1000 * 3600 * 24));
+      },
+    };
+
+(function() {
+  codeBlockCopy();
+  chageTimeFormate();
+  
+
+})();
+function getChildren(el, className) {
+  for (let item of el.children) if (item.className === className) return item;
+  return null;
+}
